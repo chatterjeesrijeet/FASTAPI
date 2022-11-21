66b18bf641f5910980f0e1bbb43d3afab3c7f846
Srijeet Chatterjee <champ.srijeet@gmail.com>
2022-11-21 17:47:55 +0000

    introducing orm

---
+++ app/database.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://<user_name>:<password>@<ip-address/hostname>/<database name>"
+
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@localhost/fastapi"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+
+Base = declarative_base()
+
+SessionLocal = sessionmaker(autocommit = False, 
+                            autoflush = False, 
+                            bind = engine)
+
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- app/main.py
+++ app/main.py
@@ -1,12 +1,19 @@
-from fastapi import FastAPI, Response, status, HTTPException
+from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
 
-import psycopg2
 import time
+
+import psycopg2
 from psycopg2.extras import RealDictCursor
+
+from sqlalchemy.orm import Session
+from . import models
+from .database import engine, SessionLocal, get_db
+
+models.Base.metadata.create_all(bind = engine)
 
 
 app = FastAPI()
@@ -20,11 +27,11 @@ class Post(BaseModel):
 while True:
     try:
         conn = psycopg2.connect(
-                                    host = "localhost",
-                                    database = "fastapi",
-                                    user = "postgres",
-                                    password = "1234",
-                                    cursor_factory = RealDictCursor
+            host = "localhost",
+            database = "fastapi",
+            user = "postgres",
+            password = "1234",
+            cursor_factory = RealDictCursor
         )
         cursor = conn.cursor()
         print("Database connected successfully!!!")
@@ -35,21 +42,17 @@ while True:
         print("Error:",error)
         time.sleep(2)
 
-def find_post(id):
-    for ele in my_posts:
-        if ele["id"] == id:
-            return ele
-    return None
-
-def find_index_post(id):
-    for idx,ele in enumerate(my_posts):
-        if ele["id"] == id:
-            return idx
-    return -1
 
 @app.get("/")
 def hello():
     return {"message" :"This is my first app"}
+
+
+@app.get("/sqlalchemy")
+def test_posts(db : Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+    return {"data" : posts}
+
 
 @app.get("/posts")
 def get_posts():
@@ -57,47 +60,56 @@ def get_posts():
     posts  =  cursor.fetchall()
     return {"data":posts}
 
+
 @app.post("/posts",status_code = status.HTTP_201_CREATED)
 def create_posts(post : Post):
-    post_dict = post.dict()
-    post_dict["id"] = randrange(0,999999)
-    my_posts.append(post_dict)
-    return {"data" : post_dict}
+    cursor.execute( """ INSERT INTO posts (title,content,published) VALUES (%s,%s,%s) RETURNING * """,
+                    (post.title,post.content,post.published)
+                   )
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {"data" : new_post }
 
-@app.get("/posts/{post_id}")
-def get_post(post_id : int, response : Response):
-    post = find_post(int(post_id))
+
+@app.get("/posts/{id}")
+def get_post(id : int, response : Response):
+
+    cursor.execute( """ SELECT * FROM posts WHERE id = %s """,
+                    (str(id)) )
+    post = cursor.fetchone()
+    print(post)
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"post with id : {post_id} does not exist.")
+                            detail = f"post with id : {id} does not exist.")
         # response.status_code = status.HTTP_404_NOT_FOUND
         # return {"message" : f"post with id : {post_id} does not exist."}
-    print(post)
+    return {"post detail ": post}
 
-    return {"post detail ":post}
 
-@app.delete("/posts/{post_id}",status_code = status.HTTP_204_NO_CONTENT)
-def delete_post(post_id : int):
-    idx = find_index_post(int(post_id))
-    if idx == -1:
+@app.delete("/posts/{id}",status_code = status.HTTP_204_NO_CONTENT)
+def delete_post(id : int):
+    cursor.execute("""DELETE FROM posts WHERE id = %s returning *""", (str(id)))
+    deleted_post = cursor.fetchone()
+    conn.commit()
+
+    if deleted_post == None:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
-                            detail = f"The post with id : {post_id} does not exist")
-    else:
-        my_posts.pop(idx)
+                            detail = f"The post with id : {id} does not exist")
 
     return Response(status_code = status.HTTP_204_NO_CONTENT)
 
 
-@app.put("/posts/{post_id}")
-def update_post(post_id:int, post:Post):
-
-    idx = find_index_post(int(post_id))
-    if idx == -1:
+@app.put("/posts/{id}")
+def update_post(id:int, post:Post):
+    cursor.execute("""UPDATE posts SET title = %s, content = %s, published=%s WHERE id = %s RETURNING *""",
+                    (post.title, post.content, post.published, str(id)))
+    updated_post = cursor.fetchone()
+    conn.commit()
+    
+    if updated_post == None:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
-                            detail = f"The post with id : {post_id} does not exist")
-    post_dict = post.dict()
-    post_dict["id"] = post_id
-    my_posts[idx] = post_dict
-
+                            detail = f"The post with id : {id} does not exist")
+    
     return {"message" : "New post successfully added",
-            "data" : post_dict}
+            "data" : updated_post}
+
